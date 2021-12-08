d1c930b0b4a238f6c9b0cab0ef11fe7f48078e24
Corentin Delorme <codelorme@gmail.com>
2021-12-08 18:45:34 +0100

    Update fixjava.sh: Don't fail when files don't exist

--- scripts/fixjava.sh
+++ scripts/fixjava.sh
@@ -9,13 +9,23 @@ if ! [ -z "${1}" ]; then
 	SOURCEDIR="${1}"
 fi
 
-sed -i ${BSDSED} \
-	-e 's|\((float)var[0-9]* \* (1.0F - (float)var[0-9]*\.getPerkLevel(PerkFactory\.Perks\.Aiming) / 15\.0F);\)$|// \1|' \
-	"${SOURCEDIR}/zombie/ai/states/SwipeStatePlayer.java"
+FILE="${SOURCEDIR}/zombie/ai/states/SwipeStatePlayer.java"
+if [ -f "${FILE}" ]; then
+	sed -i ${BSDSED} \
+		-e 's|\((float)var[0-9]* \* (1.0F - (float)var[0-9]*\.getPerkLevel(PerkFactory\.Perks\.Aiming) / 15\.0F);\)$|// \1|' \
+		"${FILE}"
+else
+	echo "File not found: ${FILE}"
+fi
 
-sed -i ${BSDSED} \
-	-e 's|<invokedynamic>||' \
-	"${SOURCEDIR}/zombie/characters/IsoPlayer.java"
+FILE="${SOURCEDIR}/zombie/characters/IsoPlayer.java"
+if [ -f "${FILE}" ]; then
+	sed -i ${BSDSED} \
+		-e 's|<invokedynamic>||' \
+		"${FILE}"
+else
+	echo "File not found: ${FILE}"
+fi
 
 # BSD sed BRE implementation is broken, so we have to use ERE here.
 # For a testcase see:
@@ -24,10 +34,20 @@ sed -i ${BSDSED} \
 #	-e 's/(Object\[\])(\(.*\)))\(\[[0-9]\]\)\?;$/new Object[]{ \1 }\2);/' \
 #	"${SOURCEDIR}/zombie/Lua/LuaManager.java" \
 #	"${SOURCEDIR}/zombie/network/GameServer.java"
-sed -i ${BSDSED} \
-	-E -e 's/\(Object\[\]\)\((.*)\)\)(\[[0-9]\])?;$/new Object[]{ \1 }\2);/' \
-	"${SOURCEDIR}/zombie/Lua/LuaManager.java" \
-	"${SOURCEDIR}/zombie/network/GameServer.java"
+
+FILES=("${SOURCEDIR}/zombie/Lua/LuaManager.java"
+"${SOURCEDIR}/zombie/network/GameServer.java")
+
+for FILE in "${FILES[@]}"; do
+
+	if [ -f "${FILE}" ]; then
+		sed -i ${BSDSED} \
+			-E -e 's/\(Object\[\]\)\((.*)\)\)(\[[0-9]\])?;$/new Object[]{ \1 }\2);/' \
+			"${FILE}"
+	else
+		echo "File not found: ${FILE}"
+	fi
+done
 
 # <= 40.43
 FILE="${SOURCEDIR}/zombie/scripting/commands/Lua/LuaCall.java"
