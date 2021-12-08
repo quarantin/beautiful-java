092f1558724fafc0d8697dc5a22b79889cd23850
Corentin Delorme <codelorme@gmail.com>
2021-12-08 18:56:45 +0100

    Add back DumperVisitor.java, it was removed by mistake

---
+++ src/beautifuljava/DumperVisitor.java
@@ -0,0 +1,22 @@
+package beautifuljava;
+
+public class DumperVisitor extends VariableVisitor {
+
+	private boolean dumpMissing;
+
+	public DumperVisitor(boolean dumpMissing) {
+		super();
+		this.dumpMissing = dumpMissing;
+	}
+
+	public void substitute(String oldName, String type) {
+
+		boolean isMissing = isMissingSymbol(oldName);
+
+		if (dumpMissing && isMissing)
+			setenv(new Symbol(type, oldName, null));
+
+		else if (!dumpMissing && !isMissing)
+			setenv(new Symbol(type, null, oldName));
+	}
+}
