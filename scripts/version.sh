7972b0e9ef77fcfe7d1472f96b89ff2c20391a5d
Corentin Delorme <codelorme@gmail.com>
2025-01-05 11:33:19 +0100

    Update scripts/version.h: Fix parsing of Zomboid version

--- scripts/version.sh
+++ scripts/version.sh
@@ -15,7 +15,7 @@ cp "${ZOMBOID}/zombie/core/Core.class" "${TMPDIR}"
 
 . "${REPO}/scripts/decompile.sh" "${TMPDIR}" "${TMPDIR}" > /dev/null
 
-VERSION="$(grep -o 'new GameVersion(.*' "${TMPDIR}/Core.java" | cut -f2 -d'(' | cut -f1,2 -d',' | sed 's/, /./')"
+VERSION="$(grep -o 'new GameVersion(.*' "${TMPDIR}/Core.java" | head -n 1 | cut -f2 -d'(' | cut -f1,2 -d',' | sed 's/, /./')"
 if [ -z "${VERSION}" ]; then
 	VERSION="$(grep 'versionNumber =' "${TMPDIR}/Core.java" | cut -f2 -d= | cut -f2 -d'"')"
 fi
